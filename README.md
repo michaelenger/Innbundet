56131881f4f36220ed0f4246299418ec2daf52dc
Michael Enger <git@michaelenger.com>
2024-01-15 15:40:37 +0000

    Add example data with the migrate command

--- README.md
+++ README.md
@@ -20,6 +20,12 @@ Migrate the database models (this only needs to be done once):
 innbundet migrate
 ```
 
+Add the `--include-example-data` flag to include some RSS feeds:
+
+```shell
+innbundet migrate --include-example-data
+```
+
 Run the web app using the `server` command:
 
 ```shell
--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,12 +1,16 @@
 package cmd
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/michaelenger/innbundet/db"
 	"github.com/michaelenger/innbundet/models"
 	"github.com/spf13/cobra"
 )
+
+// Whether we want to include example data
+var includeExampleData bool
 
 // Run the migrate command
 func runMigrateCommand(cmd *cobra.Command, args []string) {
@@ -23,6 +27,31 @@ func runMigrateCommand(cmd *cobra.Command, args []string) {
 	logger.Print("Migrating database models")
 	manager.AutoMigrate(&models.Feed{})
 	manager.AutoMigrate(&models.FeedItem{})
+
+	if includeExampleData {
+		logger.Print("Adding example data")
+		manager.Create(&models.Feed{
+			Url:         "https://michaelenger.com/feed.rss",
+			Title:       "Michael Enger",
+			Link:        sql.NullString{"https://michaelenger.com", true},
+			Description: sql.NullString{"I am a professional. This is my website.", true},
+			Image:       sql.NullString{"https://michaelenger.com/assets/happybass.png", true},
+		})
+		manager.Create(&models.Feed{
+			Url:         "https://pluralistic.net/feed/",
+			Title:       "Pluralistic: Daily links from Cory Doctorow",
+			Link:        sql.NullString{"https://pluralistic.net/", true},
+			Description: sql.NullString{"No trackers, no ads. Black type, white background. Privacy policy: we don't collect or retain any data at all ever period.", true},
+			Image:       sql.NullString{"https://i0.wp.com/pluralistic.net/wp-content/uploads/2020/02/cropped-guillotine-French-Revolution.jpg?fit=32%2C32&#038;ssl=1", true},
+		})
+		manager.Create(&models.Feed{
+			Url:         "https://boilingsteam.com/feed/rss-feed.xml",
+			Title:       "Boiling Steam",
+			Link:        sql.NullString{"https://boilingsteam.com", true},
+			Description: sql.NullString{"PC Gaming on Linux is so Tomorrow!", true},
+			Image:       sql.NullString{"", false},
+		})
+	}
 }
 
 // Migrate command - migrate database models
@@ -35,5 +64,7 @@ var migrateCommand = &cobra.Command{
 
 // Initialise the migrate command
 func init() {
+	migrateCommand.Flags().BoolVarP(&includeExampleData, "include-example-data", "d", false, "Include example data")
+
 	rootCmd.AddCommand(migrateCommand)
 }
