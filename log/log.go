c8a08f399c90bf67ba93e41a94537ae478cc739f
Michael Enger <git@michaelenger.com>
2024-03-23 19:50:59 +0000

    Implement log levels

--- log/log.go
+++ log/log.go
@@ -12,31 +12,53 @@ const (
 	DebugLevel
 )
 
+var logLevel Level = DebugLevel
+
 func SetGlobalLevel(level Level) {
-	fmt.Printf("TODO: Set log level to %d", level)
+	logLevel = level
 }
 
 // Output debug-level log message
 func Debug(format string, args ...interface{}) {
+	if logLevel < DebugLevel {
+		return
+	}
+
 	fmt.Printf(format+"\n", args...)
 }
 
 // Output error-level log message
 func Error(format string, args ...interface{}) {
+	if logLevel < ErrorLevel {
+		return
+	}
+
 	fmt.Printf(format+"\n", args...)
 }
 
 // Output info-level log message
 func Info(format string, args ...interface{}) {
+	if logLevel < InfoLevel {
+		return
+	}
+
 	fmt.Printf(format+"\n", args...)
 }
 
 // Output info-level log message (in green)
 func Success(format string, args ...interface{}) {
+	if logLevel < InfoLevel {
+		return
+	}
+
 	fmt.Printf(format+"\n", args...)
 }
 
 // Output warning-level log message
 func Warning(format string, args ...interface{}) {
+	if logLevel < WarningLevel {
+		return
+	}
+
 	fmt.Printf(format+"\n", args...)
 }
